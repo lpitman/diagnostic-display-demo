8cc6b9680e5579099ef68d194699c9e671fcb592
Logan Pitman <pitman.logan@gmail.com>
2025-09-26 12:36:11 -0300

    Adding out of range note to observations

--- app/components/CollapsibleReportItem.tsx
+++ app/components/CollapsibleReportItem.tsx
@@ -37,9 +37,21 @@ const CollapsibleReportItem = ({ report }: CollapsibleReportItemProps) => {
                 </svg>
             </div>
 
+            
+
             {/* Collapsible Report */}
             {isOpen && (
+               
                 <div className='border-t border-gray-200 p-4'>
+                     <div className='flex bg-blue-200 border border-blue-400 px-4 py-3 mb-5 rounded relative' role='alert'>
+                        <svg className=" text-blue-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 13V8m0 8h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                        </svg>
+
+                        <span className='pl-2 pt-0.5 sm:inline font-bold text-sm text-gray-800'>
+                            It is not uncommon for results to be out of range.
+                        </span>
+                    </div>
                     <div className='grid grid-cols-3 gap-4 pb-2 border-b-2 border-gray-300 mb-2 font-bold text-gray-600'>
                         <div className="text-left">Test</div>
                         <div className="text-center">Result</div>
